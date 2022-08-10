a4bc57b9431b6c2b05184ab4b8ae74b0c07e5bef
alvin leung <alvinleung@d207-023-208-189.wireless.sfu.ca>
2022-08-10 16:36:32 -0700

    try using router api

--- contact.js
+++ contact.js
@@ -1,10 +1,16 @@
-$(document).ready(function(){
+// $(document).ready(function(){
+router.useScript(() => {
   var elementDelay = 75;
-  for (let i = 0; i < document.querySelectorAll('.contact-heading span').length; i++) {
-    document.querySelectorAll('.contact-heading span').forEach((element, i) => {
+  for (
+    let i = 0;
+    i < document.querySelectorAll(".contact-heading span").length;
+    i++
+  ) {
+    document.querySelectorAll(".contact-heading span").forEach((element, i) => {
       setTimeout(function () {
         element.style.display = "inline-block";
       }, i * elementDelay);
     });
   }
 });
+// });
