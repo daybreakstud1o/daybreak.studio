c9c94cfd032cd5cb17fe86be409e57c29c09c86a
alvinleung <alvinleung2009@gmail.com>
2022-08-12 23:41:31 -0700

    test

--- about.js
+++ about.js
@@ -282,7 +282,7 @@ function enableStickyPosition(element) {
 			console.log("observing scroll")
 			daybreakScroll.observeScroll(handleScroll);
 		}
-		daybreakScroll.unobserveScroll(handleScroll);
+		// daybreakScroll.unobserveScroll(handleScroll);
 		// hanlde hwne intersecting
 	})
 
