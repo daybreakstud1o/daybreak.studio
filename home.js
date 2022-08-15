125e63fb31f8749fd45869cba892fc48c7d332c3
Alvin Leung <alvinleung2009@gmail.com>
2022-08-15 04:49:18 -0700

    test

--- home.js
+++ home.js
@@ -310,11 +310,11 @@ daybreak.router.useScript(()=>{
 			//@ts-ignore
 			navBar.style.transitionTimingFunction = "cubic-bezier(0.5, 0, 0.75, 0)";
 			//@ts-ignore
-			navBar.style.transitionDuration = ".3s";
+			navBar.style.transitionDuration = ".2s";
 			//@ts-ignore
 			navBar.style.opacity = "0";
 			//@ts-ignore
-			navBar.style.transform = "translateY(-50%)";
+			navBar.style.transform = "translateY(-20%)";
 		})
 
 		const fadeOutOtherLinks = (linksInView) => {
