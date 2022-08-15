7a47168371ffa0d45349a0b9d86282b055b8fa67
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2022-08-14 22:18:14 -0400

    Update about.js

--- about.js
+++ about.js
@@ -66,17 +66,17 @@ daybreak.router.useScript(()=>{
 
 	slider.addEventListener('mousedown', (e) => {
 	  isDown = true;
-	  slider.classList.add('active');
+	  //slider.classList.add('active');
 	  startX = e.pageX - slider.offsetLeft;
 	  scrollLeft = slider.scrollLeft;
 	});
 	slider.addEventListener('mouseleave', () => {
 	  isDown = false;
-	  slider.classList.remove('active');
+	  //slider.classList.remove('active');
 	});
 	slider.addEventListener('mouseup', () => {
 	  isDown = false;
-	  slider.classList.remove('active');
+	  //slider.classList.remove('active');
 	});
 	slider.addEventListener('mousemove', (e) => {
 	  if(!isDown) return;
