558395d2fe3488f843aad0cc4ebad8821605efa4
Alvin Leung <alvinleung2009@gmail.com>
2022-08-14 21:56:56 -0700

    grid

--- home.js
+++ home.js
@@ -1,8 +1,12 @@
+//@ts-check
+
+//@ts-ignore
+const daybreak = window.daybreak;
 
 daybreak.router.useScript(()=>{
 	console.log("enter home");
 
-	const {createGridTemplate, createInfiniteGrid} = window.daybreak;
+	const {createGridTemplate, createInfiniteGrid, shuffleGridData} = daybreak.grid;
 
 	const CELL_EMPTY = "empty";
 	const CELL_PROJECT = "project";
@@ -39,6 +43,31 @@ daybreak.router.useScript(()=>{
 	const gridContainer = document.createElement("div");
 	document.body.appendChild(gridContainer);
 
+
+	onFullyLoaded(()=>{
+
+		
+	})
+
+	const projectDataFromHTML = readProjectDataFromHTML();
+
+  // data with multiple images
+  const cellData = projectDataFromHTML.reduce((arr, currProject) => {
+    currProject.cover.forEach((coverImageUrl) => {
+      arr.push({
+        // @ts-ignore
+        ...currProject, cover: coverImageUrl
+      })
+    })
+
+    return arr;
+  }, [])
+
+	const cellDataShuffled = shuffleGridData(cellData.reduce((arr, curr) => {
+    arr.push({ importance: curr.importance, data: curr });
+    return arr
+  }, []));
+
 	const cleanupInfiniteGrid = createInfiniteGrid({
 		cols: 8,
 		templates: gridTemplates,
@@ -72,3 +101,58 @@ daybreak.router.useScript(()=>{
 		cleanupInfiniteGrid();
 	}
 })
+
+
+function readProjectDataFromHTML() {
+	const baseElm = document.querySelector(".all-daybreak-projects");
+  const allProjects = baseElm?.querySelectorAll(".daybreak-project");
+	//@ts-ignore
+  const projectData = Array.from(allProjects).map((projectElm) => {
+    const importanceElm = projectElm.querySelector(".daybreak-project-importance");
+    const nameElm = projectElm.querySelector(".daybreak-project-name");
+    const descriptionElm = projectElm.querySelector(".daybreak-project-description");
+    const yearElm = projectElm.querySelector(".daybreak-project-year");
+    const expertiseElm = projectElm.querySelector(".daybreak-project-expertise");
+    const coverElm = projectElm.querySelector(".daybreak-project-cover");
+
+		if(!importanceElm || !nameElm ||!descriptionElm||!yearElm||!expertiseElm||!coverElm) {
+			throw "invalid element"
+		}
+
+    const importance = parseInt(importanceElm.innerHTML);
+    const name = nameElm.innerHTML;
+    const description = descriptionElm.innerHTML;
+    const year = yearElm.innerHTML;
+    const expertise = expertiseElm.innerHTML.split(",").map((str) => str.trim());
+		//@ts-ignore
+    const cover = Array.from(coverElm.children).map((elm) => (elm).src);
+
+    return {
+      importance, name, description, year, expertise, cover,
+    }
+  })
+
+  return projectData;
+}
+
+function onFullyLoaded(callback) {
+	let aborted = false;
+	const abort = ()=> {
+		aborted = true;
+	};
+	const invoke = ()=> {
+		callback();
+	}
+	
+	if (document.readyState !== "complete") {
+		window.addEventListener("load", () => {
+			if(!aborted) 
+				invoke()
+		});
+	} else {
+		//invoke right if body is loaded
+		invoke();
+	}
+
+	return abort;
+}
