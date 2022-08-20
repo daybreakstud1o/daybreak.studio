ad837df9e8ae83229bc8aa01d6592bce5499fb70
Alvin Leung <alvinleung2009@gmail.com>
2022-08-20 16:47:12 -0700

    fix

--- case-study.js
+++ case-study.js
@@ -34,7 +34,7 @@ daybreak.router.useScript(()=>{
       if(isLastElm) {
         span.innerText = str;
       } else {
-        span.innerText = `${str} f`; // str + "&nbsp;";
+        span.innerText = `${str} `; // str + "&nbsp;";
       }
       elm.appendChild(span);
     });
