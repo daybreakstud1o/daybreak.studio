4609ee24036a7e833887909a0dbdc240fcf5e456
Alvin Leung <alvinleung2009@gmail.com>
2022-08-21 22:00:01 -0700

    grid scroll

--- home.js
+++ home.js
@@ -457,7 +457,9 @@ daybreak.router.useScript(()=>{
 	
 	const menuOpenButton = document.querySelector("#menu-open");
 	const daybreakInfo = document.querySelector(".daybreak-info");
+	menuOpenButton.style.willChange = `transform`;
 	menuOpenButton.style.transition = `transform .3s cubic-bezier(.36,0,.24,.99)`;
+	daybreakInfo.style.willChange = `transform,opacity`;
 	daybreakInfo.style.transitionProperty = `transform,opacity`;
 	daybreakInfo.style.transitionTimingFunction = `cubic-bezier(.36,0,.24,.99)`;
 	daybreakInfo.style.transitionDuration = `.3s`;
