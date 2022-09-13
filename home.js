5e691330105824c90acf5ad9b426f7e2a54a2a13
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2022-09-13 14:10:33 -0400

    Update home.js

--- home.js
+++ home.js
@@ -196,16 +196,16 @@ daybreak.router.useScript(()=>{
 		projectImage.style.opacity = "0";
 		projectImage.style.transitionProperty = "opacity";
 		projectImage.style.transitionDuration = ".3s";
-		projectImage.onload = function(){
+// 		projectImage.onload = function(){
  
-					for (let i = 0; i < projectImage.length; i++) {
-						projectImage.forEach((element, i) => {
-							setTimeout(function () {
-								element.style.opacity = "1";
-							}, i * shapeLoader);
-						});
-					};};
-		//projectImage.onload = () => projectImage.style.opacity = "1";
+// 					for (let i = 0; i < projectImage.length; i++) {
+// 						projectImage.forEach((element, i) => {
+// 							setTimeout(function () {
+// 								element.style.opacity = "1";
+// 							}, i * shapeLoader);
+// 						});
+// 					};};
+		projectImage.onload = () => projectImage.style.opacity = "1";
 
 
 		return projectImage;
