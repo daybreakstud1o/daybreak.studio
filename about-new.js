aee0177a7ea74d9a6be0daf475745ffde82a73d1
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2023-04-10 16:57:45 -0400

    Update about-new.js

--- about-new.js
+++ about-new.js
@@ -22,6 +22,7 @@ daybreak.router.useScript(()=>{
 	console.log("enter about");
 
 	function animateInHero() {
+		document.querySelector('.test1').classList.add("dark")
 		document.querySelector('.nav-logo').classList.add("nav-logo--expanded")
 		document.querySelector('.daybreak-info').style.opacity = '0';
 		document.querySelector('.daybreak-info').style.transitionDelay = "0s";
@@ -55,6 +56,7 @@ daybreak.router.useScript(()=>{
 					document.querySelector('.daybreak-info').style.transitionDelay = '.25s';
 					document.querySelector('.cities-info').style.transitionDelay = '.2s';
 				})
+				document.querySelector('.test1').classList.remove("dark")
 				document.querySelector('.nav-logo').classList.remove("nav-logo--expanded");
 
 		}
