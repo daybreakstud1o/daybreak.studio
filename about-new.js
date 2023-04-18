642bd38a75f760ee14a0d8244e587e6bfd60fa76
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2023-04-18 12:27:50 -0400

    Update about-new.js

--- about-new.js
+++ about-new.js
@@ -104,7 +104,7 @@ daybreak.router.useScript(()=>{
 			  );
 
 			  document.querySelector('#'+type+'-selector .progress-bar-wrapper').style.height = height
-			  document.querySelectorAll('#'+type+ '-' + i).style.zIndex = '2'
+			  document.querySelector('#'+type+ '-' + i).style.zIndex = '2'
 		}
 		
 		var list = document.querySelectorAll('.wordmarks-wrapper .wordmark-wrapper')
