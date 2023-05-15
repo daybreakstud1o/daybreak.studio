d89841ff273b38bc18d69d6bf33feaddec5413a0
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2023-05-15 18:00:08 -0400

    Update home.js

--- home.js
+++ home.js
@@ -29,6 +29,7 @@ daybreak.router.useScript(()=>{
 				window.requestAnimationFrame(()=>{
 				})
 				document.querySelector('.nav-container').classList.remove("nav-home")
+				document.querySelector('#real-nav').style.top = '0'
 
 		}
 	}
