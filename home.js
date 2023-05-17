761f2c5dbf46535622c5438fc20d1dde1cb8a546
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2023-05-17 18:50:34 -0400

    Update home.js

--- home.js
+++ home.js
@@ -13,11 +13,11 @@ daybreak.router.useScript(()=>{
 // 		    } else if(document.querySelector('#real-nav').offsetTop > 0) {
 // 		   	var scroll = 400 - $(window).scrollTop()
 // 		    	document.querySelector('#real-nav').style.top = scroll + 'px'
- 			var navTop = document.querySelector('#sticky-nav').getBoundingClientRect().top
-			var navWidth = document.querySelector('#sticky-nav').getBoundingClientRect().width
-			console.log(navTop)
-			document.querySelector('#real-nav1').style.top = navTop + 'px'
-			document.querySelector('#real-nav1').style.width = navWidth + 'px'
+//  			var navTop = document.querySelector('#sticky-nav').getBoundingClientRect().top
+// 			var navWidth = document.querySelector('#sticky-nav').getBoundingClientRect().width
+// 			console.log(navTop)
+// 			document.querySelector('#real-nav1').style.top = navTop + 'px'
+// 			document.querySelector('#real-nav1').style.width = navWidth + 'px'
 // 		    }
 		});
 		
