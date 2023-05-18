cfedb4cc87a9c83003ca2a4db291f7f23d522bd4
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2023-05-18 12:51:55 -0400

    Update home.js

--- home.js
+++ home.js
@@ -16,8 +16,7 @@ daybreak.router.useScript(()=>{
 // 		    	document.querySelector('#real-nav').style.top = scroll + 'px'
   			var navTop = document.querySelector('#real-nav1').getBoundingClientRect().top
 // 			var navWidth = document.querySelector('#sticky-nav').getBoundingClientRect().width
- 			padTop.push(navTop)
-// 			document.querySelector('#real-nav1').style.top = navTop + 'px'
+ 			document.querySelector('.animate-test').style.top = navTop + 'px'
 // 			document.querySelector('#real-nav1').style.width = navWidth + 'px'
 // 		    }
 		});
