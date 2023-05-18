57f28e7f75aca050d1f888c6d6cd579497146443
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2023-05-18 11:43:32 -0400

    Update home.js

--- home.js
+++ home.js
@@ -13,9 +13,9 @@ daybreak.router.useScript(()=>{
 // 		    } else if(document.querySelector('#real-nav').offsetTop > 0) {
 // 		   	var scroll = 400 - $(window).scrollTop()
 // 		    	document.querySelector('#real-nav').style.top = scroll + 'px'
-  			var navTop = document.querySelector('#real-nav1').getBoundingClientRect().top
+//  			var navTop = document.querySelector('#real-nav1').getBoundingClientRect().top
 // 			var navWidth = document.querySelector('#sticky-nav').getBoundingClientRect().width
- 			console.log(navTop)
+// 			console.log(navTop)
 // 			document.querySelector('#real-nav1').style.top = navTop + 'px'
 // 			document.querySelector('#real-nav1').style.width = navWidth + 'px'
 // 		    }
@@ -34,13 +34,13 @@ daybreak.router.useScript(()=>{
 // 			}
 // 		}, 200);
 
-		return ()=>{
+		return ()=>{	
+				document.querySelector('.nav-test').style.paddingTop = document.querySelector('#real-nav1').getBoundingClientRect().top + 'px'
+				console.log(document.querySelector('#real-nav1').getBoundingClientRect().top)
 				
 				// delay one frame so that transition delay is in effect
 				window.requestAnimationFrame(()=>{
 				})
-				document.querySelector('.nav-test').style.paddingTop = document.querySelector('#real-nav1').getBoundingClientRect().top + 'px'
-				console.log(document.querySelector('#real-nav1').getBoundingClientRect().top)
 				document.querySelector('.nav-container').classList.remove("nav-home")
 //				document.querySelector('#real-nav').style.top = '0px'
 
