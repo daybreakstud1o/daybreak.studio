7dc95421b190b3e28ecf568c80571ee1e387578b
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2023-05-18 12:19:03 -0400

    Update home.js

--- home.js
+++ home.js
@@ -40,8 +40,9 @@ daybreak.router.useScript(()=>{
 				
 				// delay one frame so that transition delay is in effect
 				window.requestAnimationFrame(()=>{
+					document.querySelector('.nav-test').style.paddingTop = '0!important';
 				})
-				document.querySelector('.nav-test').style.paddingTop = '0';
+				document.querySelector('.nav-test').style.paddingTop = '0!important';
 				document.querySelector('.nav-container').classList.remove("nav-home")
 //				document.querySelector('#real-nav').style.top = '0px'
 
