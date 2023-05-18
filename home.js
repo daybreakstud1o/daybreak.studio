e1b8a4d45fcb8dfbd39c9ac7e49bec34f1615d78
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2023-05-18 11:15:17 -0400

    Update home.js

--- home.js
+++ home.js
@@ -40,6 +40,7 @@ daybreak.router.useScript(()=>{
 				window.requestAnimationFrame(()=>{
 				})
 				document.querySelector('.nav-container').classList.remove("nav-home")
+				document.querySelector('.nav-test').style.paddingTop = document.querySelector('#real-nav1').getBoundingClientRect().top
 //				document.querySelector('#real-nav').style.top = '0px'
 
 		}
