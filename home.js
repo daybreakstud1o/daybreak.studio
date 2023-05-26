d276816d88fc73ca3da5c3fa635a7288da11db40
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2023-05-26 13:59:21 -0400

    Update home.js

--- home.js
+++ home.js
@@ -8,9 +8,7 @@ daybreak.router.useScript(()=>{
 		document.querySelector('.background').classList.remove("dark")
 		document.querySelector('.nav-container').classList.remove("dark","transform")
 		document.querySelector('.nav-container').classList.add("nav-home")
-		setTimeout(function() {
-			document.querySelector('.daybreak-logo-big').classList.remove("nav-logo--minimized");
-		}, 450);
+		document.querySelector('.daybreak-logo-big').classList.remove("nav-logo--minimized");
 		document.querySelector('.nav-test').classList.remove("close","dark")
 		setTimeout(function() {
 			document.querySelector('#real-nav1').style.height = "108px"
