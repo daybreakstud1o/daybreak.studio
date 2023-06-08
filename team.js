1cb3f9c2571e5b9c9d93b22b06d157592d2f7d00
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2023-06-08 15:38:27 -0400

    Update team.js

--- team.js
+++ team.js
@@ -36,16 +36,16 @@ daybreak.router.useScript(()=>{
 // 		    document.querySelector('.animate-test').style.top = '0!important'
 // 		});
 		
-		document.querySelector('.background').classList.remove("dark")
-		document.querySelector('.nav-container').classList.remove("dark")
-		document.querySelector('.nav-test').classList.remove("dark")
+// 		document.querySelector('.background').classList.remove("dark")
+// 		document.querySelector('.nav-container').classList.remove("dark")
+// 		document.querySelector('.nav-test').classList.remove("dark")
 		
-		document.querySelector('.nav-container').classList.add("transform")
-		setTimeout(function() {
-			document.querySelector('.daybreak-logo-big').classList.add("nav-logo--minimized");
-		}, 450);
-		document.querySelector('.nav-container').classList.remove("nav-home")
-		document.querySelector('.nav-test').classList.add("close")
+// 		document.querySelector('.nav-container').classList.add("transform")
+// 		setTimeout(function() {
+// 			document.querySelector('.daybreak-logo-big').classList.add("nav-logo--minimized");
+// 		}, 450);
+// 		document.querySelector('.nav-container').classList.remove("nav-home")
+// 		document.querySelector('.nav-test').classList.add("close")
 //		setTimeout(function(){
 			document.querySelector('.content').style.transform = 'translateY(0)'
 //		}, 600)
