3aea67e9c0ca88add90b68a1a3f3bd50ea908b38
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2023-06-15 19:23:22 -0400

    Update home-about.js

--- home-about.js
+++ home-about.js
@@ -1,97 +1,97 @@
 daybreak.router.useScript(() => {
 	// javascript some how has 24 in their hour
-	function fix24HrString(str) {
-		const strSplitted = str.split(":");
-		const hr = strSplitted[0] === "24"? "00":strSplitted[0];
-		const min = strSplitted[1];
-		const sec = strSplitted[2];
+// 	function fix24HrString(str) {
+// 		const strSplitted = str.split(":");
+// 		const hr = strSplitted[0] === "24"? "00":strSplitted[0];
+// 		const min = strSplitted[1];
+// 		const sec = strSplitted[2];
 
-		return `${hr}:${min}:${sec}`
-	}
+// 		return `${hr}:${min}:${sec}`
+// 	}
 
-	function getTorontoTime() {
-		const date = new Date();
+// 	function getTorontoTime() {
+// 		const date = new Date();
 
-		// pacific time
-		const eastCoastTimeStr = date.toLocaleString("en-US", {
-			timeZone: "America/New_York",
-			hour12: false
-		})
-		return fix24HrString(eastCoastTimeStr.split(" ")[1]);
-	}
+// 		// pacific time
+// 		const eastCoastTimeStr = date.toLocaleString("en-US", {
+// 			timeZone: "America/New_York",
+// 			hour12: false
+// 		})
+// 		return fix24HrString(eastCoastTimeStr.split(" ")[1]);
+// 	}
 
-	function getSFtime() {
-		const date = new Date();
+// 	function getSFtime() {
+// 		const date = new Date();
 
-		// pacific time
-		const pacificTimeStr = date.toLocaleString("en-US", {
-			timeZone: "Europe/London",
-			hour12: false
-		})
+// 		// pacific time
+// 		const pacificTimeStr = date.toLocaleString("en-US", {
+// 			timeZone: "Europe/London",
+// 			hour12: false
+// 		})
 
 
-		return fix24HrString(pacificTimeStr.split(" ")[1]);
-	}
+// 		return fix24HrString(pacificTimeStr.split(" ")[1]);
+// 	}
 
-	const updateTime = () => {
-		// periodically update the time elements
-		const allSfTimeElm = document.querySelectorAll('.sf-time');
-		const allTorontoTimeElm = document.querySelectorAll('.toronto-time');
+// 	const updateTime = () => {
+// 		// periodically update the time elements
+// 		const allSfTimeElm = document.querySelectorAll('.sf-time');
+// 		const allTorontoTimeElm = document.querySelectorAll('.toronto-time');
 
-		const sfTime = getSFtime();
-		const torontoTime = getTorontoTime();
+// 		const sfTime = getSFtime();
+// 		const torontoTime = getTorontoTime();
 
-		allSfTimeElm.forEach((elm) => elm.innerHTML = sfTime);
-		allTorontoTimeElm.forEach((elm) => elm.innerHTML = torontoTime);
-	}
+// 		allSfTimeElm.forEach((elm) => elm.innerHTML = sfTime);
+// 		allTorontoTimeElm.forEach((elm) => elm.innerHTML = torontoTime);
+// 	}
 
-	const interval = setInterval(updateTime,1000);
-	updateTime();
+// 	const interval = setInterval(updateTime,1000);
+// 	updateTime();
 
-	weatherUpdate = (city, slang) => {
-		const xhr = new XMLHttpRequest();
-		xhr.open(
-			"GET", `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cad7ec124945dcfff04e457e76760d90`);
-			xhr.send();
-			xhr.onload = () => {
-			var data = JSON.parse(xhr.response);
-			for (let i = 0; i < document.querySelectorAll('.city-' + slang + ' .clouds').length; i++) {
+// 	weatherUpdate = (city, slang) => {
+// 		const xhr = new XMLHttpRequest();
+// 		xhr.open(
+// 			"GET", `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cad7ec124945dcfff04e457e76760d90`);
+// 			xhr.send();
+// 			xhr.onload = () => {
+// 			var data = JSON.parse(xhr.response);
+// 			for (let i = 0; i < document.querySelectorAll('.city-' + slang + ' .clouds').length; i++) {
 
-				let elm;
-				if (data.weather[0].main == 'Clouds') {
-					elm = document.querySelectorAll('.city-' + slang + ' .clouds')[0];
-				} else if (data.weather[0].main == 'Clear') {
-					elm = document.querySelectorAll('.city-' + slang + ' .clear')[0];
-				} else if (data.weather[0].main == 'Rain') {
-					elm = document.querySelectorAll('.city-' + slang + ' .rain')[0];
-				} else if (data.weather[0].main == 'Snow') {
-					elm = document.querySelectorAll('.city-' + slang + ' .snow')[0];
-				} else if (data.weather[0].main == 'Haze') {
-					elm = document.querySelectorAll('.city-' + slang + ' .haze')[0];
-				} else if (data.weather[0].main == 'Thunderstorm') {
-					elm = document.querySelectorAll('.city-' + slang + ' .thunderstorm')[0];
-				} else if (data.weather[0].main == 'Mist' || data.weather[0].main == 'Fog') {
-					elm = document.querySelectorAll('.city-' + slang + ' .mist')[0]
-				} else if (data.weather[0].main == 'Drizzle') {
-					elm = document.querySelectorAll('.city-' + slang + ' .drizzle')[0]
-				}
-				if(!elm) return;
-				elm.style.display = 'block';
-				elm.style.opacity = '0';
-				elm.style.transition = 'opacity .3s linear';
-				requestAnimationFrame(()=>{
-					elm.style.opacity = "1";
-				})
-			}
-				//console.log(data.name + ", " + `${Math.round(data.main.temp - 273.15)}°C` + ", " + data.weather[0].main + ", " + data.weather[0].description)
-		};
-	};
+// 				let elm;
+// 				if (data.weather[0].main == 'Clouds') {
+// 					elm = document.querySelectorAll('.city-' + slang + ' .clouds')[0];
+// 				} else if (data.weather[0].main == 'Clear') {
+// 					elm = document.querySelectorAll('.city-' + slang + ' .clear')[0];
+// 				} else if (data.weather[0].main == 'Rain') {
+// 					elm = document.querySelectorAll('.city-' + slang + ' .rain')[0];
+// 				} else if (data.weather[0].main == 'Snow') {
+// 					elm = document.querySelectorAll('.city-' + slang + ' .snow')[0];
+// 				} else if (data.weather[0].main == 'Haze') {
+// 					elm = document.querySelectorAll('.city-' + slang + ' .haze')[0];
+// 				} else if (data.weather[0].main == 'Thunderstorm') {
+// 					elm = document.querySelectorAll('.city-' + slang + ' .thunderstorm')[0];
+// 				} else if (data.weather[0].main == 'Mist' || data.weather[0].main == 'Fog') {
+// 					elm = document.querySelectorAll('.city-' + slang + ' .mist')[0]
+// 				} else if (data.weather[0].main == 'Drizzle') {
+// 					elm = document.querySelectorAll('.city-' + slang + ' .drizzle')[0]
+// 				}
+// 				if(!elm) return;
+// 				elm.style.display = 'block';
+// 				elm.style.opacity = '0';
+// 				elm.style.transition = 'opacity .3s linear';
+// 				requestAnimationFrame(()=>{
+// 					elm.style.opacity = "1";
+// 				})
+// 			}
+// 				//console.log(data.name + ", " + `${Math.round(data.main.temp - 273.15)}°C` + ", " + data.weather[0].main + ", " + data.weather[0].description)
+// 		};
+// 	};
 
-	weatherUpdate("toronto", "toronto");
-	weatherUpdate("london", "sf");
-	weatherUpdate("toronto", "toronto-2");
-	weatherUpdate("london", "sf-2");
-	weatherUpdate("toronto", "toronto-3");
+// 	weatherUpdate("toronto", "toronto");
+// 	weatherUpdate("london", "sf");
+// 	weatherUpdate("toronto", "toronto-2");
+// 	weatherUpdate("london", "sf-2");
+// 	weatherUpdate("toronto", "toronto-3");
 	
 	
 // 	newWeather('San%20Fransisco%2C%20California%2C%20United%20States')
@@ -127,25 +127,25 @@ daybreak.router.useScript(() => {
 // 	}
 
 
-	document.getElementById('menu-open').onclick = function openMobileNav() {
-		document.getElementById('mobile-nav').style.maxHeight = '100%'
-		// document.body.style.overflow="hidden";
-	}
-	document.getElementById('menu-close').onclick = function closeMobileNav() {
-		document.getElementById('mobile-nav').style.maxHeight = '0%'
-		// document.body.style.overflow="scroll";
-	}
+// 	document.getElementById('menu-open').onclick = function openMobileNav() {
+// 		document.getElementById('mobile-nav').style.maxHeight = '100%'
+// 		// document.body.style.overflow="hidden";
+// 	}
+// 	document.getElementById('menu-close').onclick = function closeMobileNav() {
+// 		document.getElementById('mobile-nav').style.maxHeight = '0%'
+// 		// document.body.style.overflow="scroll";
+// 	}
 
 
-	let resizeTimer;
-	const handleResize = () => {
-		document.body.classList.add("resize-animation-stopper");
-		clearTimeout(resizeTimer);
-		resizeTimer = setTimeout(() => {
-			document.body.classList.remove("resize-animation-stopper");
-		}, 400);
-	}
-	window.addEventListener("resize", handleResize);
+// 	let resizeTimer;
+// 	const handleResize = () => {
+// 		document.body.classList.add("resize-animation-stopper");
+// 		clearTimeout(resizeTimer);
+// 		resizeTimer = setTimeout(() => {
+// 			document.body.classList.remove("resize-animation-stopper");
+// 		}, 400);
+// 	}
+// 	window.addEventListener("resize", handleResize);
 
 
 	const setNavCurrent = ()=>{
