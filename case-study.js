7f839a9b35080d2308b23c3db072a11a1021d167
kiranpate1 <60791907+kiranpate1@users.noreply.github.com>
2023-06-22 10:42:55 -0400

    Update case-study.js

--- case-study.js
+++ case-study.js
@@ -50,7 +50,7 @@ daybreak.router.useScript(()=>{
 			document.querySelector('.daybreak-logo-big').classList.add("nav-logo--minimized");
 		}, 450);
 		document.querySelector('.nav-container').classList.remove("nav-home")
-		document.querySelector('.nav-test').classList.add("close", "casestudy")    
+		document.querySelector('.nav-test').classList.add("casestudy")    
 		setTimeout(function(){document.querySelector('.content').style.transform = 'translateY(0)';document.querySelector('.content').style.opacity = '1'}, 600)
 		
 		setTimeout(function() {
