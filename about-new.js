52b985762174eb447e0eaa10258ca9784c3e0416
kiranpate1 <kp8568@gmail.com>
2023-07-12 16:00:22 -0400

    updates

--- about-new.js
+++ about-new.js
@@ -7,8 +7,8 @@ daybreak.router.useScript(()=>{
 		//document.querySelector('.background').classList.add("dark")
 		//document.querySelector('.footer').classList.add("dark")
 		//document.querySelector('.nav-container').classList.add("dark","transform")
-		document.querySelector('.daybreak-logo-big').style.filter = 'invert(1)'
-		document.querySelector('.background').style.filter = 'invert(1)'
+		document.querySelector('.background').style.filter = 'invert(0.9)'
+		document.querySelector('#real-nav1').classList.add("dark")
 		document.querySelector('.nav-container').classList.add("transform")
 		document.querySelector('html').classList.remove('open')
 		
