066b0490d5d1b06bc36f08077b002c8224b2368b
kiranpate1 <kp8568@gmail.com>
2023-07-26 19:36:30 -0400

    api changes

--- case-study.js
+++ case-study.js
@@ -86,11 +86,13 @@ daybreak.router.useScript(()=>{
 					var mapObj = {"\n\n":'</div><br><div class="body-founders white _100">',":":':</div><div class="body-founders white _50">'};
 					var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
 					var creditsStylized = '<div class="body-founders white _100">'+data[i].Credits.replace(re, function(matched){return mapObj[matched]})+'</div>'
+					var mapObj1 = {"\n\n":'<br>'};
+					var re1 = new RegExp(Object.keys(mapObj1).join("|"),"gi");
 					document.getElementById('casestudy-name').innerHTML = data[i].Name
 					document.getElementById('casestudy-categories').innerHTML = data[i].Categories
 					document.getElementById('intro-text').innerHTML = data[i].Intro
-					document.getElementById('problem-text').innerHTML = data[i].Problem
-					document.getElementById('solution-text').innerHTML = data[i].Solution
+					document.getElementById('problem-text').innerHTML = data[i].Problem.replace(re1, function(matched){return mapObj[matched]})
+					document.getElementById('solution-text').innerHTML = data[i].Solution.replace(re1, function(matched){return mapObj[matched]})
 					document.getElementById('casestudy-nav-credits-content').innerHTML = creditsStylized
 					if (i == '0') {
 						document.getElementById('casestudy-previous').innerHTML = data[projects.length - 1].Name
