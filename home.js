b7b8810c4dadd3a05ea4f0842be16a7ffc14f68b
kiranpate1 <kp8568@gmail.com>
2023-07-28 14:16:03 -0400

    fdfdss

--- home.js
+++ home.js
@@ -189,13 +189,12 @@ daybreak.router.useScript(()=>{
 		
 		// console.log(window.scrollY, navHeight)
 		if ((window.scrollY > [navHeight - 10]) && (window.scrollY < [navHeight + 10])) {
-			console.log('crossing')
+			var i = 0
+			console.log('crossing ' + i++)
 			if (document.querySelector('.nav-test').classList.contains("short") == false) {
-				console.log('down')
 				// document.querySelector('.nav-test').classList.add("short")
 				// document.querySelector('.daybreak-logo-big').classList.add('nav-logo--minimized')
 			} else if (document.querySelector('.nav-test').classList.contains("short") == true) {
-				console.log('up')
 				// document.querySelector('.nav-test').classList.remove("short")
 				// document.querySelector('.daybreak-logo-big').classList.remove('nav-logo--minimized')
 			}
