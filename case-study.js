0047832b4cdca3ab82fc93755533967ab6595845
Kiran Patel <60791907+kiranpate1@users.noreply.github.com>
2023-10-24 13:34:05 -0400

    Update case-study.js

--- case-study.js
+++ case-study.js
@@ -17,7 +17,10 @@ daybreak.router.useScript(()=>{
 		}, 450);
 		document.querySelector('.nav-container').classList.remove("nav-home")
 		document.querySelector('.nav-test').classList.add("casestudy")    
-		setTimeout(function(){document.querySelector('.content').style.transform = 'translateY(0)';document.querySelector('.content').style.opacity = '1'}, 600)
+		setTimeout(function(){
+			document.querySelector('.content').style.transform = 'translateY(0)';
+			document.querySelector('.content').style.opacity = '1'
+		}, 600)
 		
 		// setTimeout(function() {
 	    // 		document.querySelector('.nav-test').style.height = document.querySelector(".content").offsetHeight + 'px'
