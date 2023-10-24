98d4979b4dff2e90f458b5c3dc502b72be8ff275
Kiran Patel <60791907+kiranpate1@users.noreply.github.com>
2023-10-24 15:51:35 -0400

    Update home.js

--- home.js
+++ home.js
@@ -79,9 +79,11 @@ daybreak.router.useScript(()=>{
 		document.querySelector('#real-nav1').classList.remove('flesh-top-1','flesh-top-2','flesh-top-3')
 		
 		setTimeout(function() {
+			document.querySelector('.content').style.opacity = '1'
+	    		document.querySelector('.nav-test').style.height = document.querySelector(".content").offsetHeight + 'px'
 			document.querySelector('#real-nav1').style.height = '7.5vw'
 			//document.querySelector('#real-nav1').style.maxHeight = '164px'
-			document.querySelector('.nav-test').style.height = 'auto'
+			//document.querySelector('.nav-test').style.height = 'auto'
 		}, 600)
 		setTimeout(function() {
 			document.querySelector('#real-nav1').style.transition = "0.6s ease-in-out"
